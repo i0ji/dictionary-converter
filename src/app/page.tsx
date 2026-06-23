4c603342ab64e97a462bf90f79ef74913f8df1c3
i0ji <ioji@inbox.ru>
2026-06-23 14:51:35 +0300

    chore/build: deps update; vulnerabilities check; 2.1.0 build;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import FileUploader from "@/components/FileUpLoader/FileUpLoader";
 export default function Home() {
   
   //CONSOLE
-  console.log(process.env.NODE_ENV);
+  // console.log(process.env.NODE_ENV);
+  console.log("v: 2.1.0")
 
   return (
     <main className="bg-red-500">
